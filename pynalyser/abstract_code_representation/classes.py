708160a91b8a7e8b8bea326af458981e461453e4
0dminnimda <0dminnimda@gmail.com>
2021-08-29 01:57:20 +0300

    Create classes.py

--- pynalyser/abstract_code_representation/classes.py
+++ pynalyser/abstract_code_representation/classes.py
@@ -1,4 +1,4 @@
-from typing import List, Set, Union, Any
+from typing import List, Set, Union, Any, TypeVar
 import ast
 
 
@@ -9,17 +9,8 @@ class ACR:
     pass
 
 
-Action = Union[ast.AST, ACR]
-
-
-class Actor(ACR):
-    def __init__(self, *acts: Action) -> None:
-        self.acts = list(acts)
-
-
-class Name(Actor):
-    def __init__(self, name: str, *acts: Action) -> None:
-        super().__init__(*acts)
+class Name(ACR):
+    def __init__(self, name: str) -> None:
         self.name = name
 
     def __hash__(self) -> int:
@@ -29,35 +20,81 @@ class Name(Actor):
         return self.name == other
 
 
-class ActionSpace(ACR, Set[Name]):
-    def __getitem__(self, key: str) -> Name:
+NameT = TypeVar("NameT", bound=Name)
+
+
+class NameSpace(Set[NameT], ACR):
+    def __getitem__(self, key: str) -> NameT:
         # for set str is the same as Name
         # because of __eq__ and __hash__
-        result: Set[Name] = {key}.intersection(self)  # type: ignore
+        result: Set[NameT] = {key} & self  # type: ignore
 
         # if len (result) == 0, then it ignores the loop,
-        # otherwise return the only possible element
+        # otherwise return one only possible element
         for i in result:
             return i
 
         raise KeyError(key)
 
 
-class Scope(Actor):
-    def __init__(self, scope: ActionSpace, *acts: Action) -> None:
-        super().__init__(*acts)
+Action = Union[ast.AST, ACR]
+
+
+class Actor(ACR):  # need separate class??
+    def __init__(self, actions: List[Action] = []) -> None:
+        self.actions = actions
+
+
+class NamedActor(Name, Actor):
+    def __init__(self, name: str, actions: List[Action] = []) -> None:
+        Name.__init__(self, name)
+        Actor.__init__(self, actions)
+
+
+class Variable(NamedActor):
+    def __init__(self, name: str, actions: List[Action] = [],
+                 scope: str = "") -> None:
         self.scope = scope
 
 
+class Scope(NamedActor):  # NamedScope
+    def __init__(self, name: str,
+                 actions: List[Action] = [],
+                 actors: NameSpace[NamedActor] = NameSpace(),
+                 scopes: NameSpace["Scope"] = NameSpace()) -> None:
+        super().__init__(name, actions)
+        self.actors = actors  # variables ???
+        self.scopes = scopes  # definitions for classes and functions
+
+    # action_space: NameSpace[NamedActor] = NameSpace(),
+    #              scopes: NameSpace[Scope] = NameSpace(),
+    # def __init__(self, name: str, *args, **kwargs) -> None:
+    #     Name.__init__(self, name)
+    #     Scope.__init__(self, *args, **kwargs)
+
+
 class Module(Scope):
+    """`name` is the name of the file that this module belongs to
+    """
     pass
-    # filename ?
 
 
 class Function(Scope):
-    pass
+    args: ast.arguments
+    returns: ast.expr
+    decorator_list: List[ast.AST]
+    # body in in the actions of Actor
+
+    # type_comment ? (i think no)
+
+
+class Lambda(Function):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__("<lambda>", *args, **kwargs)
 
 
 class Class(Scope):
-    pass
-
+    bases: List[ast.AST]  # parent-classes
+    keywords: List[ast.keyword]  # metaclass and kws for it
+    decorator_list: List[ast.AST]
+    # body and members in in the actions of Actor
