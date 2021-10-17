827e74a034fc155b9e5e3bdafca79952faf851b6
0dminnimda <0dminnimda@gmail.com>
2021-10-18 02:10:45 +0300

    Create test_acr_translator.py

---
+++ tests/test_acr_translator.py
@@ -0,0 +1,38 @@
+import ast
+import sys
+from pynalyser.acr.translation import Translator, translate_ast_to_acr
+from pynalyser.acr import classes as acr
+from collections import defaultdict
+
+[
+    ast.Expr(
+        value=ast.BinOp(
+            left=ast.Name(id='a', ctx=ast.Load()),
+            op=ast.Add(),
+            right=ast.Name(id='b', ctx=ast.Load())))]
+
+
+def test_Module():
+    tree = ast.Module(body=[], type_ignores=[])
+    name = "test"
+    acr_module = translate_ast_to_acr(tree, name)
+    assert type(acr_module) == acr.Module
+    assert acr_module.name == name
+    assert acr_module.body == []
+    assert acr_module.scopes == defaultdict(acr.ScopeDefs)
+    assert acr_module.symbol_table == acr.SymbolTable()
+
+
+def test_Expr():
+    tree = ast.Module(
+        body=[ast.Expr(value=ast.Name(id='a', ctx=ast.Load()))],
+        type_ignores=[])
+    acr_module = translate_ast_to_acr(tree, "test")
+    # TODO
+    breakpoint
+
+
+if __name__ == "__main__":
+    import pytest
+
+    pytest.main([__file__])
