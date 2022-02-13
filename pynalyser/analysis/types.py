3c9f038b50ea8ef47bc004ea1954dc30fcb5c806
0dminnimda <0dminnimda@gmail.com>
2022-02-13 19:30:08 +0300

    analysis\types.py: add inferring of binop missing types, join helper classes

--- pynalyser/analysis/types.py
+++ pynalyser/analysis/types.py
@@ -1,19 +1,41 @@
 import ast
-from typing import List, Union
+from typing import List, Tuple, Union
 from warnings import warn
 
 from ..acr import classes as acr_c
-from ..acr.utils import NodeVisitor, dump, NODE
-from ._types import (DUNDER_SIGNATURE, IntType, PynalyserType, SequenceType,
-                     SingleType, UnionType, AnyType)
+from ..acr.utils import NODE, NodeVisitor, dump
+from ._types import (DUNDER_SIGNATURE, AnyType, IntType, PynalyserType,
+                     SequenceType, SingleType, UnionType)
 from .tools import Analyser
 
+binop_s = {"Add": "+",
+           "Sub": "-",
+           "Mult": "*",
+           "MatMult": "@",
+           "Div": "/",
+           "Mod": "%",
+           "LShift": "<<",
+           "RShift": ">>",
+           "BitOr": "|",
+           "BitXor": "^",
+           "BitAnd": "&",
+           "FloorDiv": "//",
+           "Pow": "**"}
 
-# XXX: ACRExprTypeInference?
-class CompTypeInference(NodeVisitor):
+binop = {
+    name: eval(f"lambda a, b: a {op} b")
+    for name, op in binop_s.items()}
+
+
+op_to_dunder = {
+    "*": "mul"
+}
+
+
+class ExprTypeInference(NodeVisitor):
     # auto_global_visit: bool = True
 
-    def infer(self, scope: acr_c.Scope, node: acr_c.ACR) -> PynalyserType:
+    def infer(self, scope: acr_c.Scope, node: NODE) -> PynalyserType:
         res = self.start(scope, node)
         if isinstance(res, PynalyserType):
             return res
@@ -21,41 +43,22 @@ class CompTypeInference(NodeVisitor):
 
     def visit_ListComp(self, node: acr_c.ListComp) -> PynalyserType:
         return SequenceType(
-
-
-binop_s = {"Add": "+",
-         "Sub": "-",
-         "Mult": "*",
-         "MatMult": "@",
-         "Div": "/",
-         "Mod": "%",
-         "LShift": "<<",
-         "RShift": ">>",
-         "BitOr": "|",
-         "BitXor": "^",
-         "BitAnd": "&",
-         "FloorDiv": "//",
-         "Pow": "**"}
-
-binop = {
-    name: eval(f"lambda a, b: a {op} b")
-    for name, op in binop_s.items()}
-
-
-class ASTExprTypeInference(ast.NodeVisitor):
-    def infer(self, node: ast.AST) -> PynalyserType:
-        res = self.visit(node)
-        if isinstance(res, PynalyserType):
-            return res
-        return objectType
             name=list.__name__, item_type=AnyType, is_builtin=True)
 
     def visit_Attribute(self, node: ast.Attribute) -> PynalyserType:
         return AnyType
 
     def visit_BinOp(self, node: ast.BinOp) -> PynalyserType:
-        return binop[type(node.op).__name__](
+        left, right, result = self.handle_op(
+            type(node.op).__name__,
             self.visit(node.left), self.visit(node.right))
+
+        if isinstance(node.left, ast.Name):
+            self.scope.symbol_table[node.left.id].type = left
+        if isinstance(node.right, ast.Name):
+            self.scope.symbol_table[node.right.id].type = right
+
+        return result
 
     def visit_List(self, node: ast.List) -> PynalyserType:
         return SequenceType(
@@ -69,23 +72,53 @@ class ASTExprTypeInference(ast.NodeVisitor):
         if isinstance(node.value, int):
             return IntType()
         return SingleType(name=type(node.value).__name__, is_builtin=True)
-        # warn("Inferring constant not as main type is not supported. "
-        #      f"Main type is '{type(node).__name__}'")
+
+    def visit_Name(self, node: ast.Name) -> PynalyserType:
+        return self.scope.symbol_table[node.id].type
+
+    def handle_op(
+        self, op: str, left: SingleType, right: SingleType
+    ) -> Tuple[SingleType, SingleType, SingleType]:
+
+        if left is AnyType and right is AnyType:
+            raise NotImplementedError
+        elif left is AnyType:
+            raise NotImplementedError
+        elif right is AnyType:
+            lsig = left.dunder_signatures.get(op_to_dunder[binop_s[op]], None)
+            if lsig is not None:
+                # TODO: also add type that have __rX__ op
+                right = UnionType.make(*lsig)
+                assert isinstance(right, SingleType)
+            else:
+                print(left.dunder_signatures, op_to_dunder[binop_s[op]], lsig)
+                raise TypeError(
+                    f"unsupported operand type(s) for {binop_s[op]}: "
+                    f"'{left.name}' and '{right.name}'")
+        # else:
+        #     raise NotImplementedError
+        #     name = op_to_dunder[op]
+        #     lsig = left.dunder_signatures.get(name, None)
+        #     if lsig is not None:
+        #         if type(right) in lsig:
+        #             return getattr(right, name)(left)
+
+        #     name = "r" + name
+        #     rsig = right.dunder_signatures.get(name, None)
+        #     if rsig is not None:
+        #         if type(left) in rsig:
+        #             return getattr(left, name)(right)
+        #     elif lsig is None:
+        #         raise TypeError("")
+
+        return left, right, binop[op](left, right)
 
 
 class TypeInference(Analyser):
-    comp_type_inference: CompTypeInference = CompTypeInference()
-    ast_expr_type_inference: ASTExprTypeInference = ASTExprTypeInference()
+    expr_type_inference: ExprTypeInference = ExprTypeInference()
 
     def infer_acr_expr(self, node: Union[ast.AST, acr_c.ACR]) -> PynalyserType:
-        if isinstance(node, acr_c.ACR):
-            return self.comp_type_inference.infer(self.scope, node)
-
-        if isinstance(node, ast.AST):
-            return self.ast_expr_type_inference.infer(node)
-
-        raise TypeError(
-            f"'node' should be AST or ACR, got {type(node).__name__}")
+        return self.expr_type_inference.infer(self.scope, node)
 
     def mash_ast_and_type(self, node: ast.AST, tp: PynalyserType) -> None:
         if isinstance(node, ast.Name):
