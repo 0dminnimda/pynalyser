bf2a91c3bdeafebf739982322218d4cf1f6cf2c1
0dminnimda <0dminnimda@gmail.com>
2022-02-13 10:56:29 +0300

    analysis\types.py: add CompTypeInference, ASTExprTypeInference, infer_acr_expr

--- pynalyser/analysis/types.py
+++ pynalyser/analysis/types.py
@@ -8,8 +8,84 @@ from ._types import PynalyserType, UnionType, SingleType, IntType, SequenceType,
 from .tools import Analyser
 
 
+# XXX: ACRExprTypeInference?
+class CompTypeInference(NodeVisitor):
+    # auto_global_visit: bool = True
+
+    def infer(self, scope: acr_c.Scope, node: acr_c.ACR) -> PynalyserType:
+        res = self.start(scope, node)
+        if isinstance(res, PynalyserType):
+            return res
+        return objectType
+
+    def visit_ListComp(self, node: acr_c.ListComp) -> PynalyserType:
+        return SequenceType(
+            name=list.__name__, item_type=objectType, is_builtin=True)
+
+
+binop_s = {"Add": "+",
+         "Sub": "-",
+         "Mult": "*",
+         "MatMult": "@",
+         "Div": "/",
+         "Mod": "%",
+         "LShift": "<<",
+         "RShift": ">>",
+         "BitOr": "|",
+         "BitXor": "^",
+         "BitAnd": "&",
+         "FloorDiv": "//",
+         "Pow": "**"}
+
+binop = {
+    name: eval(f"lambda a, b: a {op} b")
+    for name, op in binop_s.items()}
+
+
+class ASTExprTypeInference(ast.NodeVisitor):
+    def infer(self, node: ast.AST) -> PynalyserType:
+        res = self.visit(node)
+        if isinstance(res, PynalyserType):
+            return res
+        return objectType
+
+    def visit_Attribute(self, node: ast.Attribute) -> PynalyserType:
+        return objectType
+
+    def visit_BinOp(self, node: ast.BinOp) -> PynalyserType:
+        return binop[type(node.op).__name__](
+            self.visit(node.left), self.visit(node.right))
+
+    def visit_List(self, node: ast.List) -> PynalyserType:
+        return SequenceType(
+            name=list.__name__, item_type=objectType, is_builtin=True)
+
+    def visit_Tuple(self, node: ast.Tuple) -> PynalyserType:
+        return SequenceType(
+            name=tuple.__name__, item_type=objectType, is_builtin=True)
+
+    def visit_Constant(self, node: ast.Constant) -> PynalyserType:
+        if isinstance(node.value, int):
+            return IntType()
+        return SingleType(name=type(node.value).__name__, is_builtin=True)
+        # warn("Inferring constant not as main type is not supported. "
+        #      f"Main type is '{type(node).__name__}'")
+
 
 class TypeInference(Analyser):
+    comp_type_inference: CompTypeInference = CompTypeInference()
+    ast_expr_type_inference: ASTExprTypeInference = ASTExprTypeInference()
+
+    def infer_acr_expr(self, node: Union[ast.AST, acr_c.ACR]) -> PynalyserType:
+        if isinstance(node, acr_c.ACR):
+            return self.comp_type_inference.infer(self.scope, node)
+
+        if isinstance(node, ast.AST):
+            return self.ast_expr_type_inference.infer(node)
+
+        raise TypeError(
+            f"'node' should be AST or ACR, got {type(node).__name__}")
+
     def mash_ast_and_type(self, node: ast.AST, tp: PynalyserType) -> None:
         if isinstance(node, ast.Name):
             symbol_data = self.scope.symbol_table[node.id]
