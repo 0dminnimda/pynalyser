a6be48adc54ad780f741763e93552e44e7841383
0dminnimda <0dminnimda@gmail.com>
2022-02-19 22:43:10 +0300

    analysis\types.py: upd ExprTypeInference.visit_Tuple, refactor

--- pynalyser/analysis/types.py
+++ pynalyser/analysis/types.py
@@ -4,34 +4,14 @@ from warnings import warn
 
 from ..acr import classes as acr_c
 from ..acr.utils import NODE, NodeVisitor, dump
-from ._types import (DUNDER_SIGNATURE, AnyType, IntType, PynalyserType,
-                     SequenceType, SingleType, UnionType, ListType, SliceType)
+from ._ref_types import BinOpType, ItemType, SubscriptType, SymbolType, binop_s
+from ._types import (DUNDER_SIGNATURE, AnyType, IntType, ListType,
+                     PynalyserType, SequenceType, SingleType, SliceType,
+                     TupleType, UnionType)
 from .tools import Analyser
 
-binop_s = {"Add": "+",
-           "Sub": "-",
-           "Mult": "*",
-           "MatMult": "@",
-           "Div": "/",
-           "Mod": "%",
-           "LShift": "<<",
-           "RShift": ">>",
-           "BitOr": "|",
-           "BitXor": "^",
-           "BitAnd": "&",
-           "FloorDiv": "//",
-           "Pow": "**"}
 
-binop = {
-    name: eval(f"lambda a, b: a {op} b")
-    for name, op in binop_s.items()}
-
-
-op_to_dunder = {
-    "*": "mul"
-}
-
-
+# XXX: merge into TypeInference?
 class ExprTypeInference(NodeVisitor):
     # auto_global_visit: bool = True
 
@@ -41,43 +21,35 @@ class ExprTypeInference(NodeVisitor):
             return res
         return AnyType
 
+    # def visit_Attribute(self, node: ast.Attribute) -> PynalyserType:
+    #     return AnyType
+
+    def visit_Subscript(self, node: ast.Subscript) -> PynalyserType:
+        return SubscriptType(self.visit(node.value), self.visit(node.slice))
+
+    def visit_BinOp(self, node: ast.BinOp) -> PynalyserType:
+        return BinOpType(type(node.op).__name__,
+                         self.visit(node.left), self.visit(node.right))
+
+    ### Comprehentions ###
+
     def visit_ListComp(self, node: acr_c.ListComp) -> PynalyserType:
         return ListType(item_type=AnyType)  # TODO: infer item type
 
-    def visit_Attribute(self, node: ast.Attribute) -> PynalyserType:
-        return AnyType
-
-    def visit_Subscript(self, node: ast.Subscript) -> PynalyserType:
-        value_type = self.visit(node.value)
-        slice_type = self.visit(node.slice)
-        if isinstance(value_type, SequenceType):
-            return value_type[slice_type]
-
-        raise NotImplementedError
-
-    def visit_Slice(self, node: ast.Slice) -> PynalyserType:
-        return SliceType()
-
-    def visit_BinOp(self, node: ast.BinOp) -> PynalyserType:
-        left, right, result = self.handle_op(
-            type(node.op).__name__,
-            self.visit(node.left), self.visit(node.right))
-
-        if isinstance(node.left, ast.Name):
-            self.scope.symbol_table[node.left.id].type = left
-        if isinstance(node.right, ast.Name):
-            self.scope.symbol_table[node.right.id].type = right
-
-        return result
+    ### Builtin sequences ###
 
     def visit_List(self, node: ast.List) -> PynalyserType:
         return ListType(item_type=UnionType.make(
             *map(self.visit, node.elts), fallback=AnyType))
 
     def visit_Tuple(self, node: ast.Tuple) -> PynalyserType:
-        return SequenceType(name=tuple.__name__, is_builtin=True,
-                            item_type=UnionType.make(
-                                *map(self.visit, node.elts), fallback=AnyType))
+        return TupleType(item_type=UnionType.make(
+            *map(self.visit, node.elts), fallback=AnyType))
+
+    ### Basic "building blocks"  ###
+
+    def visit_Slice(self, node: ast.Slice) -> PynalyserType:
+        return SliceType()
 
     def visit_Constant(self, node: ast.Constant) -> PynalyserType:
         if isinstance(node.value, int):
@@ -85,44 +57,7 @@ class ExprTypeInference(NodeVisitor):
         return SingleType(name=type(node.value).__name__, is_builtin=True)
 
     def visit_Name(self, node: ast.Name) -> PynalyserType:
-        return self.scope.symbol_table[node.id].type
-
-    def handle_op(
-        self, op: str, left: SingleType, right: SingleType
-    ) -> Tuple[SingleType, SingleType, SingleType]:
-
-        if left is AnyType and right is AnyType:
-            raise NotImplementedError
-        elif left is AnyType:
-            raise NotImplementedError
-        elif right is AnyType:
-            lsig = left.dunder_signatures.get(op_to_dunder[binop_s[op]], None)
-            if lsig is not None:
-                # TODO: also add type that have __rX__ op
-                right = UnionType.make(*lsig)
-                assert isinstance(right, SingleType)
-            else:
-                print(left.dunder_signatures, op_to_dunder[binop_s[op]], lsig)
-                raise TypeError(
-                    f"unsupported operand type(s) for {binop_s[op]}: "
-                    f"'{left.name}' and '{right.name}'")
-        # else:
-        #     raise NotImplementedError
-        #     name = op_to_dunder[op]
-        #     lsig = left.dunder_signatures.get(name, None)
-        #     if lsig is not None:
-        #         if type(right) in lsig:
-        #             return getattr(right, name)(left)
-
-        #     name = "r" + name
-        #     rsig = right.dunder_signatures.get(name, None)
-        #     if rsig is not None:
-        #         if type(left) in rsig:
-        #             return getattr(left, name)(right)
-        #     elif lsig is None:
-        #         raise TypeError("")
-
-        return left, right, binop[op](left, right)
+        return SymbolType(self.scope.symbol_table[node.id])
 
 
 class TypeInference(Analyser):
@@ -158,11 +93,4 @@ class TypeInference(Analyser):
 
     def visit_For(self, node: acr_c.For) -> None:
         tp = self.infer_acr_expr(node.iter)
-
-        # TODO: BAD CODE, WE NEED IterableType
-        try:
-            tp = tp.item_type
-        except AttributeError:
-            pass
-
-        self.infer_assignment(node.target, tp)
+        self.infer_assignment(node.target, ItemType(tp))
