c3ccb776634a253ebe91f454af7f31d03d96188e
0dminnimda <0dminnimda@gmail.com>
2022-03-02 02:00:04 +0300

    analysis\_types.py: add Arg, Arguments, FunctionType classes

--- pynalyser/analysis/_types.py
+++ pynalyser/analysis/_types.py
@@ -195,3 +195,25 @@ class TupleType(SequenceType):
     is_builtin: bool = True
 
     is_completed: bool = True
+
+
+@attr.s(auto_attribs=True, hash=True)
+class Arg:
+    name: str
+    type: PynalyserType
+    default: Optional[PynalyserType]
+
+
+@attr.s(auto_attribs=True, hash=True)
+class Arguments:
+    posargs: List[Arg]
+    args: List[Arg]
+    stararg: Optional[Arg]
+    kwargs: List[Arg]
+    twostararg: Optional[Arg]
+
+
+@attr.s(auto_attribs=True, hash=True)
+class FunctionType(PynalyserType):
+    args: Arguments
+    # return_type
