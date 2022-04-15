7f7465852c82ed5bb9fa46f68135fc22a4e3c93f
0dminnimda <0dminnimda@gmail.com>
2022-04-16 00:40:29 +0300

    Move NameCollector to analysers\tools.py

--- pynalyser/analysers/scope.py
+++ pynalyser/analysers/scope.py
@@ -5,34 +5,7 @@ from .. import acr
 from .. import portable_ast as ast
 from ..symbol import ScopeType
 from ..types import Arg, Arguments, FunctionType, SymbolTableType
-from .tools import Analyser, AnalysisContext
-
-
-class NameCollector(acr.NodeVisitor):
-    # accounted for:
-    # Attribute - don't visit fields, XXX: for now
-    # Subscript - don't visit fields
-    # Starred - just visit itself
-    # Name - add name, if we got here
-    # List - just visit itself
-    # Tuple - just visit itself
-    # other nodes AFAIK will not appear here
-
-    _collected_names: List[str] = []
-
-    def collect_names(self, node: ast.AST) -> List[str]:
-        self._collected_names.clear()
-        self.visit(node)
-        return self._collected_names
-
-    def visit_Attribute(self, node: ast.Attribute) -> None:
-        pass
-
-    def visit_Subscript(self, node: ast.Subscript) -> None:
-        pass
-
-    def visit_Name(self, node: ast.Name) -> None:
-        self._collected_names.append(node.id)
+from .tools import Analyser, AnalysisContext, NameCollector
 
 
 # XXX: split into creation of symtab and symbols
--- pynalyser/analysers/tools.py
+++ pynalyser/analysers/tools.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List
 import attr
 
 from .. import acr
+from .. import portable_ast as ast
 
 
 @attr.s(auto_attribs=True)
@@ -17,3 +18,30 @@ class Analyser(acr.NodeVisitor):
     def analyse(self, ctx: AnalysisContext) -> None:
         self.context = ctx
         self.start(ctx.modules[0])
+
+
+class NameCollector(acr.NodeVisitor):
+    # accounted for:
+    # Attribute - don't visit fields, XXX: for now
+    # Subscript - don't visit fields
+    # Starred - just visit itself
+    # Name - add name, if we got here
+    # List - just visit itself
+    # Tuple - just visit itself
+    # other nodes AFAIK will not appear here
+
+    _collected_names: List[str] = []
+
+    def collect_names(self, node: ast.AST) -> List[str]:
+        self._collected_names.clear()
+        self.visit(node)
+        return self._collected_names
+
+    def visit_Attribute(self, node: ast.Attribute) -> None:
+        pass
+
+    def visit_Subscript(self, node: ast.Subscript) -> None:
+        pass
+
+    def visit_Name(self, node: ast.Name) -> None:
+        self._collected_names.append(node.id)
