dbce9ff4947fb89eeedb5ede9e37566f61054b22
0dminnimda <0dminnimda@gmail.com>
2022-08-02 17:08:13 +0300

    :sparkles: normalize_ast.py: pass locations from the previous  nodes

--- pynalyser/normalize_ast.py
+++ pynalyser/normalize_ast.py
@@ -1,22 +1,42 @@
-from typing import cast
+from typing import Any, Dict, Optional, cast
 from . import portable_ast as ast
+
+ATTRIBUTES = ("lineno", "col_offset")
+OPTIONAL_ATTRIBUTES = ("end_lineno", "end_col_offset")
 
 
 class AstNormalizer(ast.NodeTransformer):
+    # we need to set all of the locations,
+    # that's why ast.copy_location is not used
+    def get_locations(self, node: ast.AST) -> Dict[str, Optional[int]]:
+        result: Dict[str, Optional[int]] = {}
+
+        for attr in ATTRIBUTES:
+            value = getattr(node, attr)
+            assert isinstance(value, int)
+            result[attr] = value
+
+        for attr in OPTIONAL_ATTRIBUTES:
+            value = getattr(node, attr, None)
+            assert isinstance(value, int) or value is None
+            result[attr] = value
+
+        return result
+
     def visit_Num(self, node: ast.Num) -> ast.Constant:
-        return ast.Constant(node.n)
+        return ast.Constant(node.n, **self.get_locations(node))
 
     def visit_Str(self, node: ast.Str) -> ast.Constant:
-        return ast.Constant(node.s)
+        return ast.Constant(node.s, **self.get_locations(node))
 
     def visit_Bytes(self, node: ast.Bytes) -> ast.Constant:
-        return ast.Constant(node.s)
+        return ast.Constant(node.s, **self.get_locations(node))
 
     def visit_NameConstant(self, node: ast.NameConstant) -> ast.Constant:
-        return ast.Constant(node.value)
+        return ast.Constant(node.value, **self.get_locations(node))
 
     def visit_Ellipsis(self, node: ast.Ellipsis) -> ast.Constant:
-        return ast.Constant(...)
+        return ast.Constant(..., **self.get_locations(node))
 
     def visit_Index(self, node: ast.Index) -> ast.AST:
         value = node.value  # type: ignore [name-defined]
@@ -25,6 +45,7 @@ class AstNormalizer(ast.NodeTransformer):
     def visit_ExtSlice(self, node: ast.ExtSlice) -> ast.Tuple:
         dims = node.dims  # type: ignore [name-defined]
         return self.visit(ast.Tuple(dims, ast.Load()))
+        # **self.get_locations(first node with locations from dims)
 
     # We are just getting rid of deprecated nodes,
     # so we don't need a warning (see: NodeTransformer.visit_Constant)
