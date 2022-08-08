156f7f55a288a1858dd3e25f7d7e8be2a14268c8
0dminnimda <0dminnimda@gmail.com>
2022-08-08 19:59:59 +0300

    ✨types\exceptions.py: add unops, bad_unary(), fix unsupported_op()

--- pynalyser/types/exceptions.py
+++ pynalyser/types/exceptions.py
@@ -21,14 +21,23 @@ OP_TO_STR = {
     "le": "<=",
     "gt": ">",
     "ge": ">=",
+
+    # unop
+    "invert": "~",
+    "pos": "+",
+    "neg": "-"
 }
 
 
 def unsupported_op(op: str, lhs: str, rhs: str) -> TypeError:
     return TypeError(
-        f"unsupported operand type(s) for '{OP_TO_STR[op]}': '{lhs}' and '{rhs}'"
+        f"unsupported operand type(s) for {OP_TO_STR[op]}: '{lhs}' and '{rhs}'"
     )
 
 
 def not_subscriptable(type: str) -> TypeError:
     return TypeError(f"'{type}' object is not subscriptable")
+
+
+def bad_unary(op: str, type: str) -> TypeError:
+    return TypeError(f"bad operand type for unary {OP_TO_STR[op]}: '{type}'")
