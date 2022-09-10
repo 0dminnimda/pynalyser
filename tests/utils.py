597524740ec4e87748eac937649dfd8a53699ff0
0dminnimda <0dminnimda@gmail.com>
2022-09-11 00:53:37 +0300

    👷utils.py: add raises_instance() and fix_message()

--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,17 @@
 Things that simplify the testing process and help with it
 """
 
+import re
+
 import pytest
+
+
+def fix_message(message: str):
+    return "^" + re.escape(message).replace("\\ ", " ") + "$"
+
+
+def raises_instance(exc: Exception):
+    return pytest.raises(type(exc), match=fix_message(exc.args[0]))
 
 
 def do_test(file: str) -> None:
