c84d23259f92a1e80faebcdf97e516b6ed3f5e7e
0dminnimda <0dminnimda@gmail.com>
2022-09-25 19:26:59 +0300

    📝Use sphinx-book-theme and set html_title explicitly

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_book_theme"
+html_title = pynalyser.__name__.capitalize()
+
 html_static_path = ["_static"]
 
 html_logo = "_static/logo_icon.svg"
